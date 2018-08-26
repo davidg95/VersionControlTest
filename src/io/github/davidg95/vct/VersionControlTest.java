d57851e78ed615defa333d42b0aee8569a6a2430
david <david@DAVID-PC.mshome.net>
2018-08-26 12:37:04 +0100

    Initial commit with message box

---
+++ src/io/github/davidg95/vct/VersionControlTest.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.github.davidg95.vct;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author david
+ */
+public class VersionControlTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Version Control Test");
+    }
+    
+}
